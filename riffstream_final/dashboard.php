5af152e9bbe90c767ed2e4331b4e0ef86f6606f4
yelofoot <127456751+yelofoot@users.noreply.github.com>
2025-11-24 15:34:31 -0500

    Add profile update, playlist deletion, and artist track flows

--- riffstream_final/dashboard.php
+++ riffstream_final/dashboard.php
@@ -249,6 +249,11 @@ $flash_msg = isset($_GET['msg']) ? $_GET['msg'] : '';
           </div>
 
           <div class="row">
+            <a class="btn" href="update_profile.php">Edit profile</a>
+            <a class="btn btn-secondary" href="delete_playlist.php">Manage playlists</a>
+            <?php if ($isArtist): ?>
+              <a class="btn" href="add_track.php">Add track</a>
+            <?php endif; ?>
             <a class="btn" href="logout.php">Log out</a>
           </div>
         </section>
